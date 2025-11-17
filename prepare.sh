d8e49d90b0593a60cf06237c5cd5840b4cc221cf
bifferos <bifferos@users.noreply.github.com>
2025-11-17 10:47:54 +0000

    Add prepare script

---
+++ prepare.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Prepare the environment for building, assumes a new alpine-chroot, or docker github action container.
+# This is a one-time thing doesn't need to be run each build.
+
+echo "Adding packages"
+apk add --no-cache abuild ethtool build-base
+echo "Addding user biff"
+adduser -D biff
+addgroup biff abuild
+echo "Changing ownership of workspace"
+chown -R biff:biff ${GITHUB_WORKSPACE:-$(pwd)}
+mkdir -p /home/biff/.abuild
+echo "Copying private key from root"
+cp /root/.abuild/signing.key /home/biff/.abuild/.
+chown biff:biff /home/biff/.abuild/signing.key
+chmod 600 /home/biff/.abuild/signing.key
+echo "Writing key to build config"
+echo 'PACKAGER_PRIVKEY="/home/biff/.abuild/signing.key"' > /home/biff/.abuild/abuild.conf
+chown biff:biff /home/biff/.abuild/abuild.conf
+chmod 644 /home/biff/.abuild/abuild.conf
+
+echo "Getting public key from sources"
+cp .abuild/signing.key.pub /home/biff/.abuild/
+echo "Trusting public key on build machine"
+cp .abuild/signing.key.pub /etc/apk/keys/
+chmod 644 /etc/apk/keys/signing.key.pub
+
+chown biff:biff /home/biff/.abuild/signing.key.pub
+chmod 644 /home/biff/.abuild/signing.key.pub
