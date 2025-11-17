36e026975682c27490d85852c1eddde2c07af482
bifferos <bifferos@users.noreply.github.com>
2025-11-17 16:25:37 +0000

    Add echo

--- lanwakeable.sh
+++ lanwakeable.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Enable Wake-on-LAN for all ethernet interfaces
 for iface in $(ls /sys/class/net/ | grep -E '^e(n|th)'); do
+    echo "Setting WOL for $iface"
     /usr/sbin/ethtool -s "$iface" wol g
 done
